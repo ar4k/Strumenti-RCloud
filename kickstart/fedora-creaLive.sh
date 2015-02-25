d51a55db903ed838e5d5ecdb81a9610b612d1853
Andrea Ambrosini <andrea.ambrosini@rossonet.com>
2015-02-26 00:04:06 +0100

    Configurazione meccanismo ks Openshift

--- kickstart/fedora-creaLive.sh
+++ kickstart/fedora-creaLive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # crea il dvd da ks
 
-livecd-creator --debug --verbose --config=$1 --fslabel=Rossonet --cache=/var/cache/live
+livecd-creator --debug --verbose --config=$1 --fslabel=Rossonet --cache=/var/cache/live --releasever=21
